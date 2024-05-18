6a512b477b5952520262d57a1ba30c96378b505b
FadlyAbd <fadlyherdyanto@gmail.com>
2024-05-18 13:25:56 +0700

    update routing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+app.set('view engine', 'html');
 const { EventEmitter } = require('events');
 const emitter = new EventEmitter();
 
