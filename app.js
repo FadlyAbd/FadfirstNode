23c3889544c3c277c8f5b8bd38217f77a726289e
FadlyAbd <fadlyherdyanto@gmail.com>
2024-05-18 13:07:04 +0700

    Membenarkan eventemitter

--- app.js
+++ app.js
@@ -7,19 +7,17 @@ const emitter = new EventEmitter();
 emitter.on('sectionOpen', (sectionId) => {
     console.log(`Section ${sectionId} opened`);
 })
-// Middleware buat serve static files (CSS, JS, Images, etc.)
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Route buat halaman HTML
+ 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/home', (req, res)=> {
-    res.sendFile(path.join(__dirname,  'public', 'index.html'))
-});
-// Jalankan server di port 3000
+app.use((req, res, next) => {
+    res.status(404).send("Halaman tidak ditemukan");
+  });
+ 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servernya jalan :)`));
