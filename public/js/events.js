53d651bc4142a180d622e537895c41ed08d85118
FadlyAbd <fadlyherdyanto@gmail.com>
2024-05-19 09:21:12 +0700

    EventEmitter

--- public/js/events.js
+++ public/js/events.js
@@ -1,24 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const sections = document.querySelectorAll('section');
+ 
+  document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
 
-  sections.forEach(section => {
-    let observer;
+    sections.forEach(section => {
+      let observer;
 
-    const handleIntersection = (entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
+      const handleIntersection = (entries) => {
+        entries.forEach(entry => {
           const cards = entry.target.querySelectorAll('.card');
-          cards.forEach(card => {
-            card.classList.remove('slide-in'); 
-            setTimeout(() => {
-              card.classList.add('slide-in'); 
-            }, 100);
-          });
-        }
-      });
-    };
+          const texts = entry.target.querySelectorAll('.animated-text');
+          if (entry.isIntersecting) {
+            cards.forEach(card => {
+              card.classList.add('slide-in');
+              card.classList.remove('slide-out');
+            });
+            texts.forEach(text => {
+              text.classList.add('slide-in-x');
+              text.classList.remove('slide-out-x');
+            });
+          } else {
+            cards.forEach(card => {
+              card.classList.remove('slide-in');
+              card.classList.add('slide-out');
+            });
+            texts.forEach(text => {
+              text.classList.remove('slide-in-x');
+              text.classList.add('slide-out-x');
+            });
+          }
+        });
+      };
 
-    observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
-    observer.observe(section);
+      observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
+      observer.observe(section);
+    });
   });
-});
